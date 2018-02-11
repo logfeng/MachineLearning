4324292474bc960da94cab500cdaee79353ea976
logfeng <544387346@qq.com>
2018-02-11 17:52:47 +0800

    去除n_target ，修改loss_func ，修改数据读取方式（未完成）

--- pythonTest/code/Spectrum_classifier.py
+++ pythonTest/code/Spectrum_classifier.py
@@ -13,10 +13,10 @@ BATCH_SIZE = 50
 LR = 0.001  # learning rate
 BAIS_INIT = 0
 
-n_target = 5
+# n_target = 5
 n_fold = 20
-X = np.load("train_data.npy").astype(np.float32)
-Y = np.load("train_label.npy").astype(np.float32)
+X = np.load("train_data.npy").astype(np.float32)  # data
+Y = np.load("train_label.npy").astype(np.float32) # label
 
 wavenumber = np.load("x_name.npy")
 
@@ -52,7 +52,7 @@ class CNN_classifier(nn.Module):
             nn.Dropout(0.5),
         )
         self.out = nn.Sequential(
-            nn.Linear(2048, 1671),        # fully connected layer, output miner classes
+            nn.Linear(2048, 1671),        # fully connected layer, output probability of miner classes [.1, .2, .7] unnormalized
             nn.BatchNorm1d(momentum=0.4, num_features=1671),
             nn.Softmax(1),
         )
@@ -71,50 +71,51 @@ class CNN_classifier(nn.Module):
         return output, x
 
 
-train_loss = np.zeros(n_target * n_fold)
-valid_loss = np.zeros(n_target * n_fold)
+train_loss = np.zeros(n_fold)
+valid_loss = np.zeros(n_fold)
 
 # arrange data
-for t in range(n_target):
+for f in range(n_fold):
+    print("Target %d Fold %d --------------------------------------------" % f)
+    train_data, test_data, train_label, test_label = train_test_split(X, Y, test_size=0.05)
+    train_data = Data.TensorDataset(torch.unsqueeze(torch.FloatTensor(train_data), dim=1),
+                                    torch.FloatTensor(train_label))
+    train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+    test_y = Variable(torch.FloatTensor(test_label))
+    test_x = Variable(torch.unsqueeze(torch.FloatTensor(test_data), dim=1))
+    # net work instance
+    cnn = CNN_classifier()
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)  # optimize all cnn parameters
+    loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
 
-    YY = Y[:, t]
+    for epoch in range(EPOCH):
+        for step, (x, y) in enumerate(train_loader):  # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(x)  # batch x
+            b_y = Variable(y).type(torch.FloatTensor)  # batch y
 
-    for f in range(n_fold):
-        print("Target %d Fold %d --------------------------------------------" % (t, f))
-        train_data, test_data, train_label, test_label = train_test_split(X, YY, test_size=0.05)
-        train_data = Data.TensorDataset(torch.unsqueeze(torch.FloatTensor(train_data), dim=1),
-                                        torch.FloatTensor(train_label))
-        train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-        test_y = Variable(torch.FloatTensor(test_label))
-        test_x = Variable(torch.unsqueeze(torch.FloatTensor(test_data), dim=1))
-        # net work instance
-        cnn = CNN_classifier()
-        optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)  # optimize all cnn parameters
-        loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
+            output = cnn(b_x)[0]  # cnn output
+            loss = loss_func(output, b_y)  # cross entropy loss
+            optimizer.zero_grad()  # clear gradients for this training step
+            loss.backward()  # backpropagation, compute gradients
+            optimizer.step()  # apply gradients
 
-        for epoch in range(EPOCH):
-            for step, (x, y) in enumerate(train_loader):  # gives batch data, normalize x when iterate train_loader
-                b_x = Variable(x)  # batch x
-                b_y = Variable(y).type(torch.FloatTensor)  # batch y
+            if step == 15:
+                test_output, _ = cnn(test_x)
+                prediction = torch.max(test_output, 1)[1]
+                pred_y = prediction.data.numpy().squeeze()
+                target_y = y.data.numpy()
+                cel = loss_func(test_output, test_y)
+                accuracy = sum(pred_y == target_y)/200.  # denominator has to change
+                print('Epoch: ', epoch,
+                      '| Train loss: %.4f' % loss.data[0],
+                      '| Test loss: %.4f' % cel.data.numpy()[0],
+                      '| Accuracy loss: %.4f' % accuracy)
 
-                output = cnn(b_x)[0]  # cnn output
-                loss = loss_func(output, b_y)  # cross entropy loss
-                optimizer.zero_grad()  # clear gradients for this training step
-                loss.backward()  # backpropagation, compute gradients
-                optimizer.step()  # apply gradients
+    print("SAVING LOSS.....................................")
+    train_loss[t * n_fold + f] = loss.data.numpy()[0]
+    valid_loss[t * n_fold + f] = mse.data.numpy()[0]
+    np.save("train_loss.npy", train_loss)
+    np.save("valid_loss.npy", valid_loss)
 
-                if step == 15:
-                    test_output, _ = cnn(test_x)
-                    mse = loss_func(test_output, test_y)
-                    print('Epoch: ', epoch,
-                          '| Train loss: %.4f' % loss.data[0],
-                          '| Test loss: %.4f' % mse.data.numpy()[0])
-
-        print("SAVING LOSS.....................................")
-        train_loss[t * n_fold + f] = loss.data.numpy()[0]
-        valid_loss[t * n_fold + f] = mse.data.numpy()[0]
-        np.save("train_loss.npy", train_loss)
-        np.save("valid_loss.npy", valid_loss)
-
-        print("SAVING MODELS...................................")
-        torch.save(cnn, 'target_%d_fold_%d_train_%.4f_valid_%.4f.pkl' % (t, f, train_loss[t * n_fold + f], valid_loss[t * n_fold + f]))
+    print("SAVING MODELS...................................")
+    torch.save(cnn, 'target_%d_fold_%d_train_%.4f_valid_%.4f.pkl' % (t, f, train_loss[t * n_fold + f], valid_loss[t * n_fold + f]))
