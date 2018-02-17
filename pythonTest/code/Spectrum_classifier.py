344c9826799a8b9182be3b9a95d16e0a0c698937
logfeng <544387346@qq.com>
2018-02-17 15:55:58 +0800

    数据resample，label比较还存在问题

--- pythonTest/code/Spectrum_classifier.py
+++ pythonTest/code/Spectrum_classifier.py
@@ -26,24 +26,24 @@ class CNN_classifier(nn.Module):
     def __init__(self, bn=False):
         super(CNN_classifier, self).__init__()
         self.do_bn = bn
-        #         self.bn_input = nn.BatchNorm1d(momentum=0.5, num_features=3578)
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=16, kernel_size=21, stride=2, padding=0),
+        #         self.bn_input = nn.BatchNorm1d(momentum=0.5, num_features=3578)           Lout=floor((Lin+2∗padding−dilation∗(kernel_size−1)−1)/stride+1)
+        self.conv1 = nn.Sequential(                                                         # input shape (1, 1, 3578) floor((Lin - kernel_size)/stride + 1)
+            nn.Conv1d(in_channels=1, out_channels=16, kernel_size=21, stride=2, padding=0), # (16, 1, 1779)
             nn.BatchNorm1d(momentum=0.4, num_features=16),
             nn.LeakyReLU(negative_slope=0.2),
-            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.MaxPool1d(kernel_size=2, stride=2),                                          # output shape (16, 1, 889)
         )
-        self.conv2 = nn.Sequential(
-            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=11, stride=2, padding=0),
+        self.conv2 = nn.Sequential(                                                         # intput shape (16, 1, 889)
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=11, stride=2, padding=0),# (32, 1, 440)
             nn.BatchNorm1d(momentum=0.4, num_features=32),
             nn.LeakyReLU(negative_slope=0.2),
-            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.MaxPool1d(kernel_size=2, stride=2),                                          # output shape (32, 1, 220)
         )
-        self.conv3 = nn.Sequential(
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=0),
+        self.conv3 = nn.Sequential(                                                         # input shape (32, 1, 220)
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=0), # (64, 1, 108)
             nn.BatchNorm1d(momentum=0.4, num_features=64),
             nn.LeakyReLU(negative_slope=0.2),
-            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.MaxPool1d(kernel_size=2, stride=2),                                          # output shape (64, 1, 54)
         )
         self.dense = nn.Sequential(
             nn.Linear(64 * 54, 2048),  # Dense(2048)
@@ -52,7 +52,7 @@ class CNN_classifier(nn.Module):
             nn.Dropout(0.5),
         )
         self.out = nn.Sequential(
-            nn.Linear(2048, 1671),        # fully connected layer, output probability of miner classes [.1, .2, .7] unnormalized
+            nn.Linear(2048, 1671),     # fully connected layer, output probability of miner classes [.1, .2, .7] unnormalized
             nn.BatchNorm1d(momentum=0.4, num_features=1671),
             nn.Softmax(1),
         )
@@ -76,46 +76,46 @@ valid_loss = np.zeros(n_fold)
 
 # arrange data
 for f in range(n_fold):
-    print("Target %d Fold %d --------------------------------------------" % f)
-    train_data, test_data, train_label, test_label = train_test_split(X, Y, test_size=0.05)
-    train_data = Data.TensorDataset(torch.unsqueeze(torch.FloatTensor(train_data), dim=1),
-                                    torch.FloatTensor(train_label))
-    train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-    test_y = Variable(torch.FloatTensor(test_label))
-    test_x = Variable(torch.unsqueeze(torch.FloatTensor(test_data), dim=1))
-    # net work instance
-    cnn = CNN_classifier()
-    optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)  # optimize all cnn parameters
-    loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
+#     print("Fold %d --------------------------------------------" % f)
+#     train_data, test_data, train_label, test_label = train_test_split(X, Y, test_size=0.05)
+#     train_data = Data.TensorDataset(torch.unsqueeze(torch.FloatTensor(train_data), dim=1),
+#                                     torch.FloatTensor(train_label))
+#     train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+#     test_y = Variable(torch.FloatTensor(test_label))
+#     test_x = Variable(torch.unsqueeze(torch.FloatTensor(test_data), dim=1))
+#     # net work instance
+#     cnn = CNN_classifier()
+#     optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)  # optimize all cnn parameters
+#     loss_func = nn.CrossEntropyLoss(weight= None)          # the target label is not one-hotted, weight is proportional to the number of samples in class C
 
-    for epoch in range(EPOCH):
-        for step, (x, y) in enumerate(train_loader):  # gives batch data, normalize x when iterate train_loader
-            b_x = Variable(x)  # batch x
-            b_y = Variable(y).type(torch.FloatTensor)  # batch y
+#     for epoch in range(EPOCH):
+#         for step, (x, y) in enumerate(train_loader):   # gives batch data, normalize x when iterate train_loader
+#             b_x = Variable(x)                          # batch x
+#             b_y = Variable(y).type(torch.FloatTensor)  # batch y
 
-            output = cnn(b_x)[0]  # cnn output
-            loss = loss_func(output, b_y)  # cross entropy loss
-            optimizer.zero_grad()  # clear gradients for this training step
-            loss.backward()  # backpropagation, compute gradients
-            optimizer.step()  # apply gradients
+#             output = cnn(b_x)[0]           # cnn output
+#             loss = loss_func(output, b_y)  # cross entropy loss
+#             optimizer.zero_grad()          # clear gradients for this training step
+#             loss.backward()                # backpropagation, compute gradients
+#             optimizer.step()               # apply gradients
 
-            if step == 15:
-                test_output, _ = cnn(test_x)
-                prediction = torch.max(test_output, 1)[1]
-                pred_y = prediction.data.numpy().squeeze()
-                target_y = y.data.numpy()
-                cel = loss_func(test_output, test_y)
-                accuracy = sum(pred_y == target_y)/200.  # denominator has to change
-                print('Epoch: ', epoch,
-                      '| Train loss: %.4f' % loss.data[0],
-                      '| Test loss: %.4f' % cel.data.numpy()[0],
-                      '| Accuracy loss: %.4f' % accuracy)
+#             if step == 15:
+#                 test_output, _ = cnn(test_x)
+#                 prediction = torch.max(test_output, 1)[1]
+#                 pred_y = prediction.data.numpy().squeeze()
+#                 target_y = y.data.numpy()
+#                 cel = loss_func(test_output, test_y)
+#                 accuracy = sum(pred_y == target_y)/1671.  # denominator has to change
+#                 print('Epoch: ', epoch,
+#                       '| Train loss: %.4f' % loss.data[0],
+#                       '| Test loss: %.4f' % cel.data.numpy()[0],
+#                       '| Accuracy loss: %.4f' % accuracy)
 
-    print("SAVING LOSS.....................................")
-    train_loss[f] = loss.data.numpy()[0]
-    valid_loss[f] = cel.data.numpy()[0]
-    np.save("train_loss.npy", train_loss)
-    np.save("valid_loss.npy", valid_loss)
+#     print("SAVING LOSS.....................................")
+#     train_loss[f] = loss.data.numpy()[0]
+#     valid_loss[f] = cel.data.numpy()[0]
+#     np.save("train_loss.npy", train_loss)
+#     np.save("valid_loss.npy", valid_loss)
 
-    print("SAVING MODELS...................................")
-    torch.save(cnn, 'fold_%d_train_%.4f_valid_%.4f.pkl' % (f, train_loss[f], valid_loss[f]))
+#     print("SAVING MODELS...................................")
+#     torch.save(cnn, 'fold_%d_train_%.4f_valid_%.4f.pkl' % (f, train_loss[f], valid_loss[f]))
--- pythonTest/code/readMineralData.py
+++ pythonTest/code/readMineralData.py
@@ -1,46 +1,111 @@
 # -*- coding: utf-8 -*-
 import os
 import csv
-import numpy as np  
-import pandas as pd 
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+import math
+import matplotlib.pyplot as plt
 
-filepath = '../data/one/'
+filepath = '../data/excellent_unoriented/'
 # filepath = '../data/excellent_unoriented/'
 files = os.listdir(filepath)
 pieces = []
-specify_str = '780'
+specify_str = '532'
+maxSpectrum = 1800
+minSpectrum = 85
+numSpectrum = maxSpectrum - minSpectrum + 1
+speciesTmp = ''
+label = 0
+# preprocess, resample
 for file in files:
 	if os.path.splitext(file)[1] == '.txt' and specify_str in file:
 		print ("------Load " + file + "------")
 		dataPath = filepath + file
-		print file.split('__')[0]
+		species = file.split('__')[0]
+		print species
+		print speciesTmp
+		print (species is speciesTmp)
+		if species is speciesTmp :
+			pass
+		else :
+			label = label + 1
+		print label
+		speciesTmp = species
 		txt = np.loadtxt(dataPath, delimiter = ', ')
 		# print txt
 		txtDF = pd.DataFrame(txt, columns = list('AB')) # 用波数代替columns list('AB')
 		# print txtDF
-		txtTmp = pd.DataFrame(txt, index = txtDF['A'])
-		# print txtTmp
-		txtDF1 = pd.DataFrame(txtTmp.iloc[:,1])
-		# print txtDF1
-		pieces.append(txtDF1.T)
-		# c = np.vstack((a,b)) # 纵向合并
+		x = txtDF['A'].T
+		y = txtDF['B'].T
+		xmax = math.floor(max(x))
+		xmin = math.ceil(min(x))
+		if (xmin < minSpectrum) :
+			xmin = minSpectrum
+		if (xmax > maxSpectrum) :
+			xmax = maxSpectrum
+		xnum = xmax - xmin + 1
+		xnew = np.linspace(xmin, xmax, xnum)
+		f = interpolate.interp1d(x,y,kind = 'cubic')
+		ynew = f(xnew)
+
+		xres = np.linspace(minSpectrum, maxSpectrum, numSpectrum)
+		xmin = int(xmin)
+		xmax = int(xmax)
+		res1 = list(np.zeros(xmin - minSpectrum))
+		res2 = list(np.zeros(maxSpectrum - xmax))
+		resample = res1 + list(ynew) + res2
+
+		resample.append(label)
+		resample.append(species)
+		resTmp = pd.DataFrame(resample)
+		pieces.append(resTmp.T)
+
 outputs = pd.concat(pieces, ignore_index = True)
-# print outputs
 outputs.to_csv('../data/one/file.csv', index = False)
 
-#load data
-# file=open("D:\pythonTest\data\one\Abelsonite__R070007__Raman__532__0__unoriented__Raman_Data_Processed__27040.txt")
-# lines=file.readlines()
-# rows=len(lines)
-# print rows
+# 直接根据波数合并，不合理，弃用
+# for file in files:
+# 	if os.path.splitext(file)[1] == '.txt' and specify_str in file:
+# 		print ("------Load " + file + "------")
+# 		dataPath = filepath + file
+# 		print file.split('__')[0]
+# 		txt = np.loadtxt(dataPath, delimiter = ', ')
+# 		# print txt
+# 		txtDF = pd.DataFrame(txt, columns = list('AB')) # 用波数代替columns list('AB')
+# 		# print txtDF
+# 		txtTmp = pd.DataFrame(txt, index = txtDF['A'])
+# 		# print txtTmp
+# 		txtDF1 = pd.DataFrame(txtTmp.iloc[:,1])
+# 		# print txtDF1
+# 		pieces.append(txtDF1.T)
+# 		# c = np.vstack((a,b)) # 纵向合并
+# outputs = pd.concat(pieces, ignore_index = True)
+# # print txtDF
 
-# datamat=np.zeros((rows,2))
+# # print outputs
+# outputs.to_csv('../data/one/file.csv', index = False)
 
-# row=0
-# for line in lines:
-#     line=line.strip(', ').split('\t')
-#     datamat[row,:]=line[:]
-#     row+=1
+# resample test
+# x = txtDF['A'].T
+# y = txtDF['B'].T
+# xmax = math.floor(max(x))
+# xmin = math.ceil(min(x))
+# xnum = xmax - xmin + 1
+# xnew = np.linspace(xmin, xmax, xnum)
 
-# print(datamat)
-# print(datamat.shape)
+# plt.figure()
+# plt.plot(x,y,color='red')
+
+# f = interpolate.interp1d(x,y,kind = 'cubic')
+# ynew = f(xnew)
+
+# xres = np.linspace(85, 1800, 1716)
+# xmin = int(xmin)
+# xmax = int(xmax)
+# res1 = list(np.zeros(xmin-85))
+# res2 = list(np.zeros(1800-xmax))
+# resample = res1 + list(ynew) + res2
+
+# plt.plot(xres,resample,'k*')
+# plt.show()
