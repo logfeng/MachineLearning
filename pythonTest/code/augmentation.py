81668eb59b6c6002786a26020a1d0cda0b454d67
logfeng <544387346@qq.com>
2018-03-16 16:11:54 +0800

    数据填充，并删除样本数为1 的矿石样本

--- pythonTest/code/augmentation.py
+++ pythonTest/code/augmentation.py
@@ -8,11 +8,14 @@ import matplotlib.pyplot as plt
 X = np.loadtxt("../data/train_data.csv", delimiter = ',', skiprows = 1)     # data
 Y = np.loadtxt("../data/train_label.csv", delimiter = ',', skiprows = 1)    # label
 Z = np.loadtxt("../data/train_classNum.csv", delimiter = ',', skiprows = 1) # classNum
-length = X.shape[1]  # colume
+length = X.shape[1]    # colume
 mineral = X.shape[0]
-augEach = 10         # shift number of spectrum in each class
+augEach = 10           # shift number of spectrum in each class
 classNum = Z.shape[0]
 augData = copy.deepcopy(X)
+augLabel = copy.deepcopy(Y)
+i = 0                  # index
+singleMinerIndex = []  # get rid of the data.
 
 # augmentaion
 # offset
@@ -25,8 +28,9 @@ for x, y in zip(X, Y) :
 	y = int(y)
 	if y % 500 == 0 :
 		print("Class %d --------------------------------------------" % y)
-		print(y)
-	if Z[y] < 2 or Z[y] >= augEach :
+	if Z[y] < 2 :
+		singleMinerIndex = np.concatenate((singleMinerIndex, [i]))
+	elif Z[y] >= augEach :
 		pass
 	else :
 		shift_num = augEach - Z[y]
@@ -39,11 +43,20 @@ for x, y in zip(X, Y) :
 			augLeft[0][0:(length - s2[s])] = x[s2[s]:length]
 			augRight[0][s2[s]:length] = x[0:(length - s2[s])]
 			augData = np.concatenate((augData, augRight), axis=0)
+			augLabel = np.concatenate((Y, [y]))
+	i = i + 1
+	print (i)
 # plt.figure()
 # plt.plot(X[2566])
 # plt.show()
-data = pd.DataFrame(X)
+
+singleMinerIndex.to_csv('../data/one/singleMinerIndex.csv', index = False)
+augData = np.delete(augData, singleMinerIndex, axis=0)
+augLabel = np.delete(augLabel, singleMinerIndex, axis=0)
+data = pd.DataFrame(augData)
+dataLabel = pd.DataFrame(augLabel)
 data.to_csv('../data/one/augmentation.csv', index = False)
+dataLabel.to_csv('../data/one/augLabel.csv', index = False)
 
 # gauss random noise, proportional to the magnitude at each wave number
 # different amplitude maybe need normalize
